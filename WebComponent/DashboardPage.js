5865e42ff3218e5900b5f1387c621928c9bd9530
strangers <138708064+malfinmajid@users.noreply.github.com>
2024-12-28 07:42:23 +0700

    Page Model Object Task

--- WebComponent/DashboardPage.js
+++ WebComponent/DashboardPage.js
@@ -15,6 +15,12 @@ class DashboardPage {
         return title.getText();
     }
 
+    
+    async navigateToCart(){
+        const cartButton = await this.driver.findElement(By.xpath("//span[@class='shopping_cart_badge']"));
+        return cartButton.click();
+    }
+
     async addItemToCart() {
         await this.driver.findElement(this.addToCartButtonBackpack).click();
         await this.driver.findElement(this.addToCartButtonBikelight).click();
