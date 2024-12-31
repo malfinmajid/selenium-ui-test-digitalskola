04a337abcfbb2a6b88cc2574d115793640763681
strangers <138708064+malfinmajid@users.noreply.github.com>
2024-12-31 13:24:55 +0700

    parallel-test learning example

--- test/TestCase7.js
+++ test/TestCase7.js
@@ -19,7 +19,7 @@ if (!fs.existsSync(screenshotDir)){
     fs.mkdirSync(screenshotDir, {recursive: true});
 }
   
-describe('TestCase 6 [finish checkout] #Regression', function () {
+describe('TestCase 7 [finish checkout] #Regression', function () {
     this.timeout(40000);
     let driver;
     // Testing on Multiple Browser
